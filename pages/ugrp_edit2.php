356db374ab5efa4da0a81e297af4be63733cad17
Cas <cas@nuy.info>
2024-09-01 09:55:02 +0200

    Update ugrp_edit2.php
    Adjusted the tanle update syntax

--- pages/ugrp_edit2.php
+++ pages/ugrp_edit2.php
@@ -1,12 +1,9 @@
 <?PHP
-//require_once( '../../../core.php' );
 $edit_id			= gpc_get_int( 'edit_id' );
-$grp_table	= plugin_table('groups','Usergroups');
 $desc		= $_REQUEST['group_desc'];
 $mail		= $_REQUEST['group_mail'];
-
 // perform update
-$query = "UPDATE $grp_table set group_desc = '$desc', group_mail= '$mail'  WHERE group_id = $edit_id";
+$param = [$desc, $mail};
+$query = "UPDATE{plugin_Usergroups_groups} set group_desc = " . db_param() . ", group_mail= " . db_param() . "  WHERE group_id = $edit_id";
 $result = db_query($query);
 print_header_redirect( 'plugin.php?page=Usergroups/config' );
-
