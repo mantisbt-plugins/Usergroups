3bd1de4f056e7951e9d23a168bfec3da76a096bc
Cas <cas@nuy.info>
2024-09-01 09:49:55 +0200

    Update ugrp_add.php
    Ajusted table update syntax

--- pages/ugrp_add.php
+++ pages/ugrp_add.php
@@ -1,15 +1,15 @@
 <?PHP
 require_once( 'core.php' );
-$grp_table	= plugin_table('groups','Usergroups');
 $name		= $_REQUEST['group_name'];
 $desc		= $_REQUEST['group_desc'];
 $mail		= $_REQUEST['group_mail'];
 // first check if entry already exists
-$query= "select * from $grp_table where upper(trim(group_name))=upper(trim('$name'))";
+$query= "select * from {plugin_Usergroups_groups} where upper(trim(group_name))=upper(trim('$name'))";
 $result = db_query($query);
 $res2=db_num_rows($result);
 if ($res2 == 0){
-	$query = "INSERT INTO $grp_table ( group_name,group_desc,group_mail) VALUES ('$name', '$desc','$mail')";
+	$param = [$name, $desc, $mail];
+	$query = "INSERT INTO {plugin_Usergroups_groups} ( group_name,group_desc,group_mail ) VALUES ( " . db_param() . ", " . db_param() . ", " . db_param() . ")";
 	if(!db_query($query)){ 
 		trigger_error( ERROR_USERGROUP_EXISTS, ERROR );
 	}
