5d63830fbd84acf32b74a1d5c645875db9158174
Cas <cas@nuy.info>
2024-09-01 09:13:34 +0200

    Update manage_usergroup_add.php
    Corrected typo within table definition

--- pages/manage_usergroup_add.php
+++ pages/manage_usergroup_add.php
@@ -2,7 +2,7 @@
 $userid		= $_REQUEST['user_id'];
 $f_group_id	= gpc_get_int_array( 'group_id', array() ); 
 foreach ( $f_group_id as $groupid ) {
-	$query = "INSERT INTO {plugin_Usergroups_usergroups} ( group_id,user_id ) VALUES ('$groupid', '$userid')";
+	$query = "INSERT INTO {plugin_Usergroups_usergroup} ( group_id,user_id ) VALUES ('$groupid', '$userid')";
 	$res=db_query($query);
 }
 $link='manage_user_edit_page.php?user_id='.$userid;
