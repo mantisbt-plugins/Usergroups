11db0792d33760f00b2c73f11d3699c3fd996e33
Cas <cas@nuy.info>
2024-09-01 10:18:09 +0200

    Update ugrp_edit2.php

--- pages/ugrp_edit2.php
+++ pages/ugrp_edit2.php
@@ -3,7 +3,6 @@ $edit_id			= gpc_get_int( 'edit_id' );
 $desc		= $_REQUEST['group_desc'];
 $mail		= $_REQUEST['group_mail'];
 // perform update
-$param = [$desc, $mail};
-$query = "UPDATE{plugin_Usergroups_groups} set group_desc = " . db_param() . ", group_mail= " . db_param() . "  WHERE group_id = $edit_id";
+$query = "UPDATE {plugin_Usergroups_groups} set group_desc = '$desc', group_mail= '$mail'  WHERE group_id = $edit_id";
 $result = db_query($query);
 print_header_redirect( 'plugin.php?page=Usergroups/config' );
