41e84cbd70f6f378ac53dc2136b7cccc783d3f85
Cas <cas@nuy.info>
2024-09-01 09:42:35 +0200

    Update manage_usergroup_add.php
    Updated table update syntax

--- pages/manage_usergroup_add.php
+++ pages/manage_usergroup_add.php
@@ -2,8 +2,9 @@
 $userid		= $_REQUEST['user_id'];
 $f_group_id	= gpc_get_int_array( 'group_id', array() ); 
 foreach ( $f_group_id as $groupid ) {
-	$query = "INSERT INTO {plugin_Usergroups_usergroup} ( group_id,user_id ) VALUES ('$groupid', '$userid')";
-	$res=db_query($query);
+	$param	= [(int)$groupid, (int)$userid];
+	$query	= "INSERT INTO {plugin_Usergroups_usergroup} ( group_id,user_id ) VALUES ( " . db_param() . ", " . db_param() . ")";
+	$res	= db_query($query);
 }
 $link='manage_user_edit_page.php?user_id='.$userid;
 print_header_redirect( $link );
