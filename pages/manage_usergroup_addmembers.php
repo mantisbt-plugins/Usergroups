f25095390e7be7ee9681a33b59ba3d59df774337
Cas <cas@nuy.info>
2024-09-01 10:28:22 +0200

    Update manage_usergroup_addmembers.php

--- pages/manage_usergroup_addmembers.php
+++ pages/manage_usergroup_addmembers.php
@@ -3,8 +3,7 @@ $groupid		= $_REQUEST['group_id'];
 if (!isset($_POST['Return'])) {
 	$f_user_id	= gpc_get_int_array( 'user_id', array() ); 
 	foreach ( $f_user_id as $userid ) {
-		$param = [(int)$groupid, (int)$userid];
-		$query = "INSERT INTO {plugin_Usergroups_usergroup} ( group_id,user_id ) VALUES ( " . db_param() . ", " . db_param() . ")";
+		$query = "INSERT INTO {plugin_Usergroups_usergroup} ( group_id,user_id ) VALUES ( $groupid, $userid)";
 		$res=db_query($query);
 	}
 }
